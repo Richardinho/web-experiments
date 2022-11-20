baaf4d5d3066fc22ea3ae4b95e881f790ce29c10
Richard Hunter <richard@richardhunter.co.uk>
2022-11-20 00:44:06 +0000

    tidying up slide in navigation component

--- slide-in-nav/README.md
+++ slide-in-nav/README.md
@@ -1,12 +1,3 @@
-// uses 
+# Slide in navigation
 
-units
-vw  Safari 10 and up, IOS Safari 9 and up, IE11 and up, not opera mini
-vh  // as above
-
-css properties / functions
-transform
-translate()
-
-events
-transitionend
+menu that slides in from side
--- slide-in-nav/main.js
+++ slide-in-nav/main.js
@@ -1,63 +1,60 @@
-'use strict';
+"use strict";
 
-let slideNav = document.getElementById('sliding-nav');
-let openNavButtons = document.querySelectorAll('[data-command=open-nav]');
-let headerEl = slideNav.querySelector('.sliding-nav-header');
-let bodyEl = slideNav.querySelector('.sliding-nav-menu-items');
-let closeNavButtons = document.querySelectorAll('[data-command=close-nav]');
-let overlay = document.getElementById('overlay');
+let slideNav = document.getElementById("sliding-nav");
+let openNavButtons = document.querySelectorAll("[data-command=open-nav]");
+let headerEl = slideNav.querySelector(".sliding-nav-header");
+let bodyEl = slideNav.querySelector(".sliding-nav-menu-items");
+let closeNavButtons = document.querySelectorAll("[data-command=close-nav]");
+let overlay = document.getElementById("overlay");
 
-openNavButtons.forEach(button => {
-    button.addEventListener('click', openNav);
+openNavButtons.forEach((button) => {
+  button.addEventListener("click", openNav);
 });
 
-closeNavButtons.forEach(button => {
-    button.addEventListener('click', closeNav);
+closeNavButtons.forEach((button) => {
+  button.addEventListener("click", closeNav);
 });
 
 layoutNav();
 
-window.addEventListener('resize', layoutNav);
-
+window.addEventListener("resize", layoutNav);
 
 function layoutNav() {
-    let headerHeight = headerEl.offsetHeight;
-    let viewportHeight = window.innerHeight;
-    let bodyHeight = viewportHeight - headerHeight;
-    bodyEl.style.height = bodyHeight + 'px';
+  let headerHeight = headerEl.offsetHeight;
+  let viewportHeight = window.innerHeight;
+  let bodyHeight = viewportHeight - headerHeight;
+  bodyEl.style.height = bodyHeight + "px";
 }
 
 function openNav() {
-    freezeUnderlyingPage();
-    showOverlay();
-    slideNav.style.transform = 'translateX(0)';
+  freezeUnderlyingPage();
+  showOverlay();
+  slideNav.style.transform = "translateX(0)";
 }
 
 function closeNav() {
-    unFreezeUnderlyingPage();
-    hideOverlay();
-    slideNav.style.transform = 'translateX(100%)';
+  unFreezeUnderlyingPage();
+  hideOverlay();
+  slideNav.style.transform = "translateX(100%)";
 }
 
 function showOverlay() {
-    overlay.style.transform  = 'translateX(0)';
-    overlay.style.opacity = 0.4;
+  overlay.style.transform = "translateX(0)";
+  overlay.style.opacity = 0.4;
 }
 
 function hideOverlay() {
-    overlay.style.opacity = 0;
-    overlay.addEventListener('transitionend', function anon() {
-        overlay.removeEventListener('transitionend', anon);
-        overlay.style.transform  = 'translateX(100%)';
-    });
+  overlay.style.opacity = 0;
+  overlay.addEventListener("transitionend", function anon() {
+    overlay.removeEventListener("transitionend", anon);
+    overlay.style.transform = "translateX(100%)";
+  });
 }
 
 function freezeUnderlyingPage() {
-    document.body.style.overflow = 'hidden';
+  document.body.style.overflow = "hidden";
 }
 
 function unFreezeUnderlyingPage() {
-    document.body.style.overflow = 'auto';
+  document.body.style.overflow = "auto";
 }
-
-
