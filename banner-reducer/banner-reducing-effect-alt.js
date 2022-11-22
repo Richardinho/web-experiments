cf6d5e40f350d96051f0983a0a3ea6a276e4711b
Richard Hunter <richard@richardhunter.co.uk>
2022-11-22 01:29:06 +0000

    formatting js

--- banner-reducer/banner-reducing-effect-alt.js
+++ banner-reducer/banner-reducing-effect-alt.js
@@ -1,42 +1,39 @@
-
 function applyEffect() {
+  let reduced = false;
+  let first = true;
 
-    let reduced = false;
-    let first = true;
+  let banner = document.querySelector("[role=banner]");
 
-    let banner = document.querySelector('[role=banner]');
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      window.addEventListener("scroll", resizeBanner);
 
-    document.addEventListener( 'DOMContentLoaded', function () {
+      function resizeBanner() {
+        let pageYOffset = window.pageYOffset;
 
-
-        window.addEventListener('scroll', resizeBanner);
-
-        function resizeBanner () {
-
-            let pageYOffset = window.pageYOffset;
-
-            if (pageYOffset > 100) {
-                if (!reduced) {
-                    reduced = true;
-                    banner.classList.add('reduced');
-                }
-            } else {
-                if (reduced) {
-                    reduced = false;
-                    banner.classList.remove('reduced');
-                }
-            }
-
-            document.body.offsetHeight;
-
-            if(first) {
-                banner.style.transition = '.7s ease all';
-                first = false;
-            }
+        if (pageYOffset > 100) {
+          if (!reduced) {
+            reduced = true;
+            banner.classList.add("reduced");
+          }
+        } else {
+          if (reduced) {
+            reduced = false;
+            banner.classList.remove("reduced");
+          }
         }
-    }, false );
 
+        document.body.offsetHeight;
 
+        if (first) {
+          banner.style.transition = ".7s ease all";
+          first = false;
+        }
+      }
+    },
+    false
+  );
 }
 
 applyEffect();
